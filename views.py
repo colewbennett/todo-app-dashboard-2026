a558164374f91790a521a30fb3f4cbe2b6f05aba
Naser Al Madi <nsalmadi@colby.edu>
2026-02-24 15:15:03 -0500

    initial db model for visit and waitlist with some webpages outlined

---
+++ views.py
@@ -0,0 +1,89 @@
+from flask import Blueprint, render_template, redirect, url_for
+from flask import request
+from task import Task
+from flask_login import login_required, current_user
+from models import db, Task, User, Visit, Waitlist
+
+# Create a blueprint
+main_blueprint = Blueprint('main', __name__)
+
+
+def log_visit(page, user_id):
+    """Log a visit to a page by a user."""
+    visit = Visit(page=page, user_id=user_id)
+    db.session.add(visit)
+    db.session.commit()
+
+
+###############################################################################
+# Routes
+###############################################################################
+
+
+@main_blueprint.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@main_blueprint.route('/invitation', methods=['GET', 'POST'])
+def invitation():
+
+    if request.method == 'POST':
+        email = request.form['email']
+        # Here you would send a verification email and add to waitlist
+        print(f"Sending invitation to {email}")
+    return render_template('invitation.html')
+
+
+@main_blueprint.route('/todo', methods=['GET', 'POST'])
+@login_required
+def todo():
+    return render_template('todo.html')
+
+
+@main_blueprint.route('/api/v1/tasks', methods=['GET'])
+@login_required
+def api_get_tasks():
+    tasks = Task.query.filter_by(user_id=current_user.id).all()
+    return {
+        "tasks": [task.to_dict() for task in tasks]
+    }
+
+
+@main_blueprint.route('/api/v1/tasks', methods=['POST'])
+@login_required
+def api_create_task():
+    data = request.get_json()
+    new_task = Task(title=data['title'], user_id=current_user.id)
+    db.session.add(new_task)
+    db.session.commit()
+    return {
+        "task": new_task.to_dict()
+    }, 201
+
+
+@main_blueprint.route('/api/v1/tasks/<int:task_id>', methods=['PATCH'])
+@login_required
+def api_toggle_task(task_id):
+    task = Task.query.get(task_id)
+
+    if task is None:
+        return {"error": "Task not found"}, 404
+
+    task.toggle()
+    db.session.commit()
+
+    return {"task": task.to_dict()}, 200
+
+
+@main_blueprint.route('/remove/<int:task_id>')
+@login_required
+def remove(task_id):
+    task = Task.query.get(task_id)
+
+    if task is None:
+        return redirect(url_for('main.todo'))
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return redirect(url_for('main.todo'))
